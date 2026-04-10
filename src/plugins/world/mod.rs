66b8f4387213d82579cc16394238d63bafb3bc9e
NaimSdn <saadoune.naim.pro@gmail.com>
2026-04-10 21:21:09 +0200

    Updating | Adding camera plugin to handle camera instead of putting it inside plugin world class render, also linting code

--- src/plugins/world/mod.rs
+++ src/plugins/world/mod.rs
@@ -1,18 +1,19 @@
-mod perlin;
-mod tile;
 mod asset;
+mod perlin;
 mod render;
+mod tile;
 
-use bevy::prelude::{App, IntoScheduleConfigs, Plugin, Startup};
+use crate::plugins::world::asset::load_image;
+use crate::plugins::world::perlin::generate_world;
+use crate::plugins::world::render::render_world;
 use crate::plugins::world::tile::WorldMap;
-use asset::load_image;
-use perlin::generate_world;
-use render::render_world;
+use bevy::prelude::{App, IntoScheduleConfigs, Plugin, Startup};
 
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<WorldMap>().add_systems(Startup, (load_image, generate_world, render_world).chain());
+        app.init_resource::<WorldMap>()
+            .add_systems(Startup, (load_image, generate_world, render_world).chain());
     }
 }
