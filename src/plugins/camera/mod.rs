9c096846a91dcebe412fa13ad78d33ecb4e5a371
NaimSdn <saadoune.naim.pro@gmail.com>
2026-04-10 23:48:21 +0200

    Updating | Adding a way for the camera to follow the player

--- src/plugins/camera/mod.rs
+++ src/plugins/camera/mod.rs
@@ -1,15 +1,32 @@
-use bevy::app::{App, Plugin, Startup};
+use crate::plugins::player::player::Player;
+use bevy::app::{App, Plugin, Startup, Update};
 use bevy::camera::Camera2d;
-use bevy::prelude::Commands;
+use bevy::prelude::{Commands, Query, Transform, With, Without};
 
 pub(crate) fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+pub(crate) fn camera_follow(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera2d>, Without<Player>)>,
+) {
+    let Ok(mut camera_transform) = camera_query.single_mut() else {
+        return;
+    };
+
+    let Ok(player_transform) = player_query.single() else {
+        return;
+    };
+
+    camera_transform.translation.x = player_transform.translation.x;
+    camera_transform.translation.y = player_transform.translation.y;
 }
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_camera);
+        app.add_systems(Startup, spawn_camera)
+            .add_systems(Update, camera_follow);
     }
 }
