384301a4754cd255b402257e94e0753bebf15fe4
NaimSdn <saadoune.naim.pro@gmail.com>
2026-04-11 14:01:01 +0200

    Updating | Adding a way for the player to be displayed when moving, also adding a way for the player to press space to go up into the Y axes

--- src/plugins/input/mod.rs
+++ src/plugins/input/mod.rs
@@ -1,6 +1,7 @@
 use crate::plugins::player::player::{Player, PlayerStats, Velocity};
 use bevy::app::{App};
-use bevy::prelude::{ButtonInput, IntoScheduleConfigs, KeyCode, Plugin, Query, Res, Time, Transform, Update, With};
+use bevy::prelude::{ButtonInput, IntoScheduleConfigs, KeyCode, Plugin, Query, Res, Transform, Update, With};
+use bevy::time::Time;
 
 pub struct InputPlugin;
 
@@ -16,13 +17,27 @@ fn moving_player(
         velocity.value.x = -stats.speed;
     } else if button_input.pressed(KeyCode::KeyD) {
         velocity.value.x = stats.speed;
+    } else if button_input.pressed(KeyCode::Space) {
+        velocity.value.y = stats.speed * 2.0;
     } else {
         velocity.value.x = 0.0;
+        velocity.value.y = 0.0;
     }
+}
+
+fn apply_velocity(
+    mut query: Query<(&Velocity, &mut Transform), With<Player>>,
+    time: Res<Time>,
+) {
+    let Ok((velocity, mut transform)) = query.single_mut() else {
+        return;
+    };
+    transform.translation.x += velocity.value.x * time.delta_secs();
+    transform.translation.y += velocity.value.y * time.delta_secs();
 }
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, moving_player);
+        app.add_systems(Update, (moving_player, apply_velocity).chain());
     }
 }
