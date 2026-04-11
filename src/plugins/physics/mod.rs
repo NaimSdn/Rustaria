f0e254ee71d5bc419c5962f818dedb0c3ab7d189
NaimSdn <saadoune.naim.pro@gmail.com>
2026-04-11 14:17:29 +0200

    Updating | Adding apply_gravity function to apply gravity onto the player every update

--- src/plugins/physics/mod.rs
+++ src/plugins/physics/mod.rs
@@ -1,3 +1,11 @@
-fn apply_gravity() {
-    
+use crate::plugins::player::player::{Player, Velocity};
+use bevy::prelude::{Query, Res, Time, With};
+use crate::constants::GRAVITY;
+
+fn apply_gravity(time: Res<Time>, mut gravity_query: Query<&mut Velocity, With<Player>>) {
+    let Ok(mut velocity) = gravity_query.single_mut() else {
+        return;
+    };
+
+    velocity.value.y -= GRAVITY * time.delta_secs();
 }
