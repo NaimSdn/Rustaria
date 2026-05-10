5f8e1c622a3e72cf1cfe0851aee4a57cffadb5c1
NaimSdn <saadoune.naim.pro@gmail.com>
2026-05-10 18:39:19 +0200

    Updating | Adding bevy_rapier2D to generate collision and physics, instead of making my own.

--- src/plugins/mod.rs
+++ src/plugins/mod.rs
@@ -2,4 +2,3 @@ pub mod camera;
 pub mod player;
 pub mod world;
 pub mod input;
-pub mod physics;
--- src/plugins/world/render.rs
+++ src/plugins/world/render.rs
@@ -1,6 +1,7 @@
 use crate::plugins::world::asset::WorldTileset;
 use crate::plugins::world::tile::{TileType, WorldMap};
-use bevy::prelude::{Commands, Res, Sprite, TextureAtlas, Transform};
+use bevy::prelude::{Commands, Res, Sprite, TextureAtlas, Transform, Vec2};
+use bevy_rapier2d::prelude::{Collider, RigidBody};
 use crate::constants::TILE_SIZE;
 
 pub(crate) fn render_world(
@@ -8,17 +9,21 @@ pub(crate) fn render_world(
     world_map: Res<WorldMap>,
     world_tileset: Res<WorldTileset>,
 ) {
-    for col in 0..world_map.world_width as usize {
-        for row in 0..world_map.world_height as usize {
-            let tile_index = match world_map.world_tiles[col][row].tile_type {
-                TileType::Dirt => 0,
+    let width = world_map.world_width as usize;
+    let height = world_map.world_height as usize;
+
+    for col in 0..width {
+        for row in 0..height {
+            let tile_type = world_map.world_tiles[col][row].tile_type;
+            let tile_index = match tile_type {
+                TileType::Dirt  => 0,
                 TileType::Grass => 1,
                 TileType::Stone => 16,
-                TileType::Air => continue,
+                TileType::Air   => continue,
             };
 
-            let x = col as f32 * TILE_SIZE - (world_map.world_width as f32 * TILE_SIZE / 2.0);
-            let y = -(row as f32 * TILE_SIZE) + (world_map.world_height as f32 * TILE_SIZE / 2.0);
+            let x = col as f32 * TILE_SIZE - (width as f32 * TILE_SIZE / 2.0);
+            let y = -(row as f32 * TILE_SIZE) + (height as f32 * TILE_SIZE / 2.0);
 
             commands.spawn((
                 Transform::from_xyz(x, y, 0.0),
@@ -29,6 +34,38 @@ pub(crate) fn render_world(
                         index: tile_index,
                     },
                 ),
+            ));
+        }
+    }
+
+    // --- Colliders: one merged collider per contiguous vertical run per column ---
+    for col in 0..width {
+        let mut row = 0;
+        while row < height {
+            if !world_map.world_tiles[col][row].tile_type.is_solid() {
+                row += 1;
+                continue;
+            }
+
+            let run_start = row;
+            while row < height && world_map.world_tiles[col][row].tile_type.is_solid() {
+                row += 1;
+            }
+            let run_end = row;
+
+            let run_len = (run_end - run_start) as f32;
+
+            let x = col as f32 * TILE_SIZE - (width as f32 * TILE_SIZE / 2.0);
+            let y_top    = -(run_start as f32 * TILE_SIZE) + (height as f32 * TILE_SIZE / 2.0);
+            let y_bottom = -(( run_end - 1) as f32 * TILE_SIZE) + (height as f32 * TILE_SIZE / 2.0);
+            let y_center = (y_top + y_bottom) / 2.0;
+
+            let half_height = run_len * TILE_SIZE / 2.0;
+
+            commands.spawn((
+                Transform::from_xyz(x, y_center, 0.0),
+                RigidBody::Fixed,
+                Collider::cuboid(TILE_SIZE / 2.0, half_height),
             ));
         }
     }
